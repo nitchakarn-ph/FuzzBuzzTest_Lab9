e6765a69c2355719aeef0f1ac1cdaa8c0a128bca
Foxtails-design <littlefither@gmail.com>
2019-10-05 00:16:28 +0700

    PhaseFail
    comment ขั้นตอน fail
    ทำให้ fail ตาม expected result ใน instruction

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -1,17 +1,29 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 
 class FizzBuzzTest {
 
 	FizzBuzz fizzBuzz = new FizzBuzz();
 	String output = fizzBuzz.getBuzz(1);
-	
-	@Test
-	void test_when_input_1() {
-		assertEquals("Buzz", output);
+	@BeforeEach
+	void setUp() throws Exception {
+		
 	}
 	
+	@DisplayName("Actual1")
+	@Test
+	void ShouldBe1_Input1() {// expected =1
+		assertEquals(1, output);
+	}
+	
+	@DisplayName("Actual2")
+	@Test
+	void ShouldBe2_Input2() {//ต้องเปรียบเทียบ เป็น expected =2
+		assertEquals(2, output);
+	}
 	
 
 }
