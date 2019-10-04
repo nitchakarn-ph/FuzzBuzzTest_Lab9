ea33746beaaecf2fdade348452b4e7a69392d9b6
Foxtails-design <littlefither@gmail.com>
2019-10-05 02:08:22 +0700

    Input8

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -14,52 +14,59 @@ class FizzBuzzTest {
 		fizzBuzz = new FizzBuzz(); // Reset Value Before enter each Test
 	}
 	
-	@DisplayName("Actual1")
+	@DisplayName("INPUT1")
 	@Test
 	void ShouldBe1_Input1() {// expected =1
 		fizzBuzz.FizzBuzzTesting(1);
 		assertEquals(1,fizzBuzz.getNum());
 	}
 	
-	@DisplayName("Actual2")
+	@DisplayName("INPUT2")
 	@Test
 	void ShouldBe2_Input2() {//��ͧ���º��º �� expected =2
 		fizzBuzz.FizzBuzzTesting(2);
 		assertEquals(2,fizzBuzz.getNum());
 	}
 	
-	@DisplayName("Actual3")
+	@DisplayName("INPUT3")
 	@Test
-	void ShouldBeFizz_InputFizz() {
+	void ShouldBeFizz_Input3() {
 		fizzBuzz.FizzBuzzTesting(3);;
 		assertEquals("Fizz", fizzBuzz.getText());
 	}
 	
-	@DisplayName("Actual4")
+	@DisplayName("INPUT4")
 	@Test
 	void ShouldBe4_Input4() {
 		fizzBuzz.FizzBuzzTesting(4);
 		assertEquals(4, fizzBuzz.getNum());
 	}
 	
-	@DisplayName("Actual5")
+	@DisplayName("INPUT5")
 	@Test
-	void ShouldBeBuzz_InputBuzz() {
+	void ShouldBeBuzz_Input5() {
 		fizzBuzz.FizzBuzzTesting(5);;
 		assertEquals("Buzz", fizzBuzz.getText());
 	}
 	
-	@DisplayName("Actual6")
+	@DisplayName("INPUT6")
 	@Test
 	void ShouldBeFizz_Input6() {
 		fizzBuzz.FizzBuzzTesting(6);;
 		assertEquals("Fizz", fizzBuzz.getText());
 	}
 	
-	@DisplayName("Actual7")
+	@DisplayName("INPUT7")
 	@Test
-	void ShouldBeFizz_Input7() {
+	void ShouldBe7_Input7() {
 		fizzBuzz.FizzBuzzTesting(7);;
 		assertEquals(7, fizzBuzz.getNum());
+	}
+	
+	@DisplayName("INPUT8")
+	@Test
+	void ShouldBe8_Input8() {
+		fizzBuzz.FizzBuzzTesting(8);;
+		assertEquals(8, fizzBuzz.getNum());
 	}
 }
