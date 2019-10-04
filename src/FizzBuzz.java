38d1fdb2d546d0d918688ada98b5d4cd4f771c39
Foxtails-design <littlefither@gmail.com>
2019-10-05 01:00:00 +0700

    Revert "PhasePass"
    This reverts commit 03b8f78f94c8d47e19c4e6ee22856a71b0c4878e.

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,38 +1,16 @@
 
 public class FizzBuzz {
-	private String Text;
-	private int Num;
-	public String getBuzz() { // ถ้า หาร 5 ลงตัวได้(%5==0) Buzz
-		return "Buzz";
-	}
-	
-	public String getFizz() { // ถ้า หาร 3 ลงตัวได้(%3==0) Buzz
-		return "Fizz";
-	}
-	
-	public String getFizzBuzz() { // ถ้า หาร 5 และ 3ลงตัวได้(%5==0 && %3==0) FizzBuzz
-		return "FizzBuzz";
-	}
-	
-	public String getText() {
-		return Text;
-	}
-
-	public int getNum() {
-		return Num;
-	}
-	public void FizzBuzzTesting(int input) {
-		if(input%5==0 && input%3==0) 
-		{
-			Text=getFizzBuzz();
-		}else if (input%5==0) 
-		{
-			Text=getBuzz();
-		}else if (input%3==0) 
-		{
-			Text=getFizz();
-		}else{
-			Num=input;
+		String text;
+		int number;
+	public String getBuzz(int input) {
+		if(input/5 == 0) {
+			text =  "Buzz";
+		}else {
+			text =  null;
 		}
+		return text;
 	}
+	
+	
+
 }
--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -6,25 +6,23 @@ import org.junit.jupiter.api.DisplayName;
 
 class FizzBuzzTest {
 
-	FizzBuzz fizzBuzz;
-	
+	FizzBuzz fizzBuzz = new FizzBuzz();
+	String output = fizzBuzz.getBuzz(1);
 	@BeforeEach
 	void setUp() throws Exception {
-		fizzBuzz = new FizzBuzz(); //ทำการ reset ค่าก่อนเข้า แต่ละ test
+		
 	}
 	
 	@DisplayName("Actual1")
 	@Test
 	void ShouldBe1_Input1() {// expected =1
-		fizzBuzz.FizzBuzzTesting(1);
-		assertEquals(1,fizzBuzz.getNum());
+		assertEquals(1, output);
 	}
 	
 	@DisplayName("Actual2")
 	@Test
 	void ShouldBe2_Input2() {//ต้องเปรียบเทียบ เป็น expected =2
-		fizzBuzz.FizzBuzzTesting(2);
-		assertEquals(2,fizzBuzz.getNum());
+		assertEquals(2, output);
 	}
 	
 
