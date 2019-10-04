03b8f78f94c8d47e19c4e6ee22856a71b0c4878e
Foxtails-design <littlefither@gmail.com>
2019-10-05 00:59:15 +0700

    PhasePass
    ทำการเขียนโค้ด ให้ผ่าน โดยใช้ คำสั่งในแต่ละ Test ให้น้อย และตรมตามexpected result

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -6,23 +6,25 @@ import org.junit.jupiter.api.DisplayName;
 
 class FizzBuzzTest {
 
-	FizzBuzz fizzBuzz = new FizzBuzz();
-	String output = fizzBuzz.getBuzz(1);
+	FizzBuzz fizzBuzz;
+	
 	@BeforeEach
 	void setUp() throws Exception {
-		
+		fizzBuzz = new FizzBuzz(); //ทำการ reset ค่าก่อนเข้า แต่ละ test
 	}
 	
 	@DisplayName("Actual1")
 	@Test
 	void ShouldBe1_Input1() {// expected =1
-		assertEquals(1, output);
+		fizzBuzz.FizzBuzzTesting(1);
+		assertEquals(1,fizzBuzz.getNum());
 	}
 	
 	@DisplayName("Actual2")
 	@Test
 	void ShouldBe2_Input2() {//ต้องเปรียบเทียบ เป็น expected =2
-		assertEquals(2, output);
+		fizzBuzz.FizzBuzzTesting(2);
+		assertEquals(2,fizzBuzz.getNum());
 	}
 	
 
